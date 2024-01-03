f1756496ff2579b33669646014a348854e974ea2
megdad <megdad.elfaki@gmail.com>
2024-01-03 11:48:46 -0500

    2024-01-03 11:48:46 - test

--- m.sh
+++ m.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Check if changes exist
 if [[ $(git status --porcelain) ]]; then
     # Add all changes
     git add .
-
-    # Prompt user for commit message
-    echo -n "Enter commit message: "
-    read commit_message
-
+    
     # Prompt user for commit message
     echo -n "Enter commit message: "
     read commit_message
     
     # Commit changes with the provided message or a default one
-    git commit -m " $(date '+%Y-%m-%d %H:%M:%S') - ${commit_message: }"
+    git commit -m "$(date '+%Y-%m-%d %H:%M:%S') - ${commit_message:-No message provided}"
+    
     # Push changes to the remote repository
     git push origin master
     
